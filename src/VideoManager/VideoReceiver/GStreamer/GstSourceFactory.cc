236c66598d7d62445f87cff18c9f24c273bd8cad
Pavlo Kostiuk <kostyk.pavel.09@gmail.com>
2026-09-24 19:00:28 +0400

    style(Video): clang-format GstSourceFactory.cc and GStreamerTest.cc

--- src/VideoManager/VideoReceiver/GStreamer/GstSourceFactory.cc
+++ src/VideoManager/VideoReceiver/GStreamer/GstSourceFactory.cc
@@ -327,8 +327,8 @@ GstElement* buildRtspSource(const QString& uri, const QUrl& sourceUrl, const Con
     const QString rtspUser = sourceUrl.userName(QUrl::FullyDecoded);
     const QString rtspPassword = sourceUrl.password(QUrl::FullyDecoded);
     if (!rtspUser.isEmpty()) {
-        g_object_set(source, "user-id", rtspUser.toUtf8().constData(), "user-pw",
-                     rtspPassword.toUtf8().constData(), nullptr);
+        g_object_set(source, "user-id", rtspUser.toUtf8().constData(), "user-pw", rtspPassword.toUtf8().constData(),
+                     nullptr);
     }
     return source;
 }
@@ -413,7 +413,8 @@ GstElement* buildUdpSource(const QUrl& sourceUrl, bool isUdpH264, bool isUdpH265
 {
     const int port = sourceUrl.port();
     if (!validPort(port)) {
-        qCCritical(GstSourceFactoryLog) << "Invalid UDP port" << port << "in" << sourceUrl.toDisplayString(QUrl::RemoveUserInfo);
+        qCCritical(GstSourceFactoryLog) << "Invalid UDP port" << port << "in"
+                                        << sourceUrl.toDisplayString(QUrl::RemoveUserInfo);
         return nullptr;
     }
 
@@ -590,7 +591,8 @@ GstElement* create(const QString& uri, const Config& config)
     const bool isTcpMPEGTS = (scheme == QLatin1String("tcp"));
 
     if (!isRtsp && !isWhep && !isUdpH264 && !isUdpH265 && !isUdpMPEGTS && !isTcpMPEGTS) {
-        qCWarning(GstSourceFactoryLog) << "Unsupported URI scheme:" << scheme << "in" << sourceUrl.toDisplayString(QUrl::RemoveUserInfo);
+        qCWarning(GstSourceFactoryLog) << "Unsupported URI scheme:" << scheme << "in"
+                                       << sourceUrl.toDisplayString(QUrl::RemoveUserInfo);
         return nullptr;
     }
 
@@ -625,8 +627,8 @@ GstElement* create(const QString& uri, const Config& config)
 
         parser = gst_element_factory_make(isUdpH265 ? "h265parse" : "parsebin", "parser");
         if (!parser) {
-            qCCritical(GstSourceFactoryLog) << "gst_element_factory_make("
-                                            << (isUdpH265 ? "'h265parse'" : "'parsebin'") << ") failed";
+            qCCritical(GstSourceFactoryLog)
+                << "gst_element_factory_make(" << (isUdpH265 ? "'h265parse'" : "'parsebin'") << ") failed";
             break;
         }
 
@@ -708,7 +710,8 @@ GstElement* create(const QString& uri, const Config& config)
                 break;
             }
         } else {
-            if (!linkSourceToParser(bin, upstream, binParser, config, latencyMs, isTcpMPEGTS || isUdpMPEGTS, isRtsp || isWhep)) {
+            if (!linkSourceToParser(bin, upstream, binParser, config, latencyMs, isTcpMPEGTS || isUdpMPEGTS,
+                                    isRtsp || isWhep)) {
                 break;
             }
         }
